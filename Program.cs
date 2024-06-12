799f350587b713f7b5355edbb361a82036f871ec
Lang Hall <lhall@heckgrammar.co.uk>
2024-06-12 10:42:36 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace number_challenge_skeleton
+{
+    internal class Program
+    {
+        static long[] numberbase(long number, int Base) //function called numberbase that take two perameters
+        {
+            long[] SolvedArray = new long[2];//change this
+            SolvedArray[0]= 2; //change this
+            SolvedArray[1]= 3; // code to do stuff - change this
+            return SolvedArray; //passes out data - keep this
+        }
+        static void Main(string[] args)
+        {
+            long[] arrayButBetter= numberbase(2, 3); //create a array to hold the returned array from the funtion 
+            Console.WriteLine(arrayButBetter[0]);
+        }
+    }
+}
